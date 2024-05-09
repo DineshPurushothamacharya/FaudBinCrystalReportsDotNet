515c612bca009ecab184cd1eede19dd0b2b3fbd2
Dinesh Purushothamacharya <p.dinesh87@gmail.com>
2024-05-09 22:16:13 +0300

    configure log4net

--- Reports/Controllers/PatientBillPaymentController.cs
+++ Reports/Controllers/PatientBillPaymentController.cs
@@ -16,9 +16,12 @@ namespace Reports.Controllers
 {
     public class PatientBillPaymentController : ApiController
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         [Route("api/PatientBillPayment/GetPaymentBillNo")]
         public PatientBiillInfoList GetPaymentBillNo(String HospitalID, string RegCode, string ScheduleId)
         {
+            log.Info("Begin GetPaymentBillNo Controller");
             PatientBillPayment objPayment = new PatientBillPayment();
             PatientBillList objRequest = new PatientBillList();
             objRequest.HospitalId = int.Parse(HospitalID); // 1;
@@ -26,6 +29,7 @@ namespace Reports.Controllers
             objRequest.ScheduleID = ScheduleId; // "5891";
 
             var objPatientBillInfoList = objPayment.GettingPatientList(objRequest);
+            log.Info("End GetPaymentBillNo Controller");
 
             return objPatientBillInfoList;
         }
